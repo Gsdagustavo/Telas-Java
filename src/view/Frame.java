dc4dde3729ea9b9290bb72e646219caff662c37e
Gsdagustavo <gugadanielalvez@gmail.com>
2025-03-17 09:04:01 -0300

    made the labels

--- src/view/Frame.java
+++ src/view/Frame.java
@@ -7,8 +7,31 @@ public class Frame extends JFrame {
 
     public Frame() throws HeadlessException {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 600);
+
+        setLayout(new FlowLayout());
+
         setVisible(true);
         setEnabled(true);
+        setSize(800, 600);
+
+        add(iconLabel());
+        add(textLabel());
+    }
+
+    private JLabel textLabel() {
+        JLabel label = new JLabel("Label com texto");
+        label.setEnabled(true);
+        label.setToolTipText("Este é o label 1");
+
+        return label;
+    }
+
+    private JLabel iconLabel() {
+        Icon icon = new ImageIcon("C:\\Users\\Aluno\\Downloads\\tomates.png");
+        JLabel label = new JLabel("Label com icone", icon, SwingConstants.LEFT);
+        label.setToolTipText("Icone");
+        label.setSize(100, 100);
+
+        return label;
     }
 }
